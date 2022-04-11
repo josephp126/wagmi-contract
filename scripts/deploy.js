5e58ce90100360f0edb5512f1b941a501e66ed3f
Sergey Popinevskiy <gurza000@gmail.com>
2022-04-11 13:23:49 +0300

    Output account balance in ETH

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,12 @@
+const { ethers } = require("ethers");
 const { task } = require("hardhat/config");
 const { getAccount } = require("./helpers");
 
 
 task("check-balance", "Prints out the balance of your account").setAction(async function (taskArguments, hre) {
 	const account = getAccount();
-	console.log(`Account balance for ${account.address}: ${await account.getBalance()}`);
+    const balance = await account.getBalance();
+	console.log(`Account balance for ${account.address}: ${ethers.utils.formatEther(balance)} ETH`);
 });
 
 task("deploy", "Deploys the NFT.sol contract").setAction(async function (taskArguments, hre) {
